2e87c8f6ffcd5544d75ab7153622d378916acf0d
Pashaie <pashaie@gmail.com>
2025-11-20 22:01:06 +0330

    API Doc

--- routes/todoRoutes.ts
+++ routes/todoRoutes.ts
@@ -4,10 +4,15 @@ const router = express.Router();
 
 import { createTodo, readTodo, readTodos, updateTodo, deleteTodo } from '../controllers/todoController';
 
+//CRUD
 router.post('/', createTodo);
 router.get('/', readTodos);
 router.get('/:id', readTodo);
 router.put('/:id', updateTodo);
+//TODO: Patch
 router.delete('/:id', deleteTodo);
+
+//REST API
+// router.post('/all-done', createTodo);
 
 export default router;
